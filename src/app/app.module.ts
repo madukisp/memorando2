cfb6b72cde30d8ab1e7d2b3d4b2e598fd9f66fca
madukisp <110678035+madukisp@users.noreply.github.com>
2023-05-31 17:07:20 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { MensagemComponent } from './componentes/mensagem/mensagem.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaTarefasComponent,
+    CabecalhoComponent,
+    MensagemComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
